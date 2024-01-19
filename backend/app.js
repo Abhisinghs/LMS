898eec62f4b4485617ad42da71e99e5ef27cea1d
Abhisinghs <abhisheksingh35716@gmail.com>
2024-01-19 17:11:26 +0530

    create one api

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { config } from "dotenv";
+import course from "./routes/Course.route.js";
 
+//set path of config file
 config({
   path: "./config/config.env",
 });
+
+//make instance of express
 const app = express();
+
+//using middleware
+
+//importing and using routes
+app.use("/api/v1", course);
 
 app.use("/", (req, res) => {
   res.json({ message: "Hello Engineer" });
