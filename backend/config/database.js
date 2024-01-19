f7f1c10e3a77351f3574414e4a7046715001281e
Abhisinghs <abhisheksingh35716@gmail.com>
2024-01-19 16:33:44 +0530

    delete the file

--- backend/config/database.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectToDb=async()=>{
-   const {connection}=await mongoose.connect(process.env.MONGO_URI);
-   console.log(`MongoDb connected with ${connection.host}`);
-}
--- backend/middlewares/catchAsynError.js
+++
@@ -1,6 +0,0 @@
-// export module
-const catchAsynError = (passedFunction)=>(req,resp,next)=>{
-    Promise.resolve(passedFunction(req,resp,next)).catch(next);
-}
-
-export default catchAsynError;
